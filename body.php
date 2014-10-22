58d0db651958a7e3e8f9d82edfb317db466c7aac
whitesir <2622282984@qq.com>
2014-10-22 08:24:29 +0800

    body.php

---
+++ body.php
@@ -0,0 +1,17 @@
+<body class="home blog custom-font-enabled single-author">
+<div id="page" class="hfeed site">
+<?php 
+include 'menu.php';
+?>
+
+	<div id="main" class="wrapper">
+
+<?php 
+include 'main.php';
+?>
+<?php 
+include 'footer.php';
+?>
+
+		</div>
+		
