e8d64cc5c4878c8878a58d099a77954104d137a6
whitesir <2622282984@qq.com>
2014-10-22 08:24:53 +0800

    footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+</div>
+	
+<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+		<center>
+    <p>&copy; Hack by NZ</p>
+</center>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+</body>
+</html>
