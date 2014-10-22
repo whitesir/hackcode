f92f5d7ab853a8bd78ae51eaef5574386f24f727
whitesir <2622282984@qq.com>
2014-10-22 08:29:17 +0800

    menu.php

---
+++ menu.php
@@ -0,0 +1,14 @@
+
+	<header id="masthead" class="site-header" role="banner">
+		<hgroup>
+			<a href="../" title="Hack by NZ" rel="home"><img src="../images/logo.png" /></a>Hack by NZ</hgroup>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<div class="menu-lsb-container">
+			<ul id="menu-lsb" class="nav-menu">
+			<li id="menu-item-14" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-14"><a href="#">I'm</a></li>
+<li id="menu-item-14" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-14"><a href="#">unhappy!</a></li>
+</ul>
+</div>		</nav>
+
+</header>
