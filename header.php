be92d51d0bd665193dc9ef53c43575094313467b
whitesir <2622282984@qq.com>
2014-10-29 11:21:49 +0800

    header.php

--- header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width" />
-<title>Hack by NZ</title>
-<link rel='stylesheet' id='style'  href='./xo.css' type='text/css' media='all' />
-
-</head>
